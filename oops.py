e5dbb4c45975df8db4fcb1a3a59b3f61ca3afcef
jashwanthkondasani <jashwanthkumarreddy53@gmail.com>
2025-08-18 21:48:42 +0530

    Update oops.py

--- oops.py
+++ oops.py
@@ -175,4 +175,30 @@ print("Circle Area:", c.area())
 print("Square Area:", s.area())
 
 
+# Create an array (list)
+arr = [10, 20, 30, 40, 50]
+
+# Display array elements
+print("Array:", arr)
+
+# Access elements
+print("First element:", arr[0])
+print("Last element:", arr[-1])
+
+# Insert element
+arr.append(60)
+print("After append:", arr)
+
+# Remove element
+arr.remove(30)
+print("After removing 30:", arr)
+
+# Update element
+arr[1] = 25
+print("After updating:", arr)
+
+# Length of array
+print("Length:", len(arr))
+
+
         
