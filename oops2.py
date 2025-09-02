2dcf91692aa5469963c11030feee73f519a3ac97
jashwanthkondasani <jashwanthkumarreddy53@gmail.com>
2025-09-02 22:46:02 +0530

    Update oops2.py

--- oops2.py
+++ oops2.py
@@ -15,3 +15,49 @@ class nani:
 jash=nani("jashwanth ","actor")
 jash.do_work()
 jash.speaks()
+
+#  inheritance
+class A:
+   def feature1(self):
+        print(" feature 1 is working")
+
+   def feature2(self):
+        print("feature 2 is working")
+
+class B(A):
+    def feature3(self):
+        print(" feature 3 is working")
+
+    def feature4(self):
+        print("feature 4 is working")
+class C(B):
+    def feature5(self):
+        print(" feature 5 is working")
+
+    def feature6(self):
+        print("feature 6 is working")
+
+
+a1=A()
+a2=A()
+
+
+
+a1.feature1()
+a2.feature2()
+b1=B()
+b2=B()
+
+
+
+b1.feature1()
+b2.feature2()
+
+c1=C()
+c2=C()
+
+
+
+C1.feature1()
+C2.feature2()
+
